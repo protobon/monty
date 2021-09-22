435202030eb782b335f05deb7f91d6442fdee3fb
hippocampus3282 <3282@holbertonschool.com>
2021-09-22 11:10:46 -0700

    Project 0x19 C - Stack, Queues. LIFO & FIFO

--- pint.c
+++ pint.c
@@ -1,12 +1,16 @@
 #include "monty.h"
 
+/**
+  * pint - prints value at top of the stack
+  * @stack: doubly linked list
+  * @ln: line number
+  */
 void pint(stack_t **stack, unsigned int ln)
 {
-	if (!stack)
+	if (!stack || !*stack)
 	{
 		fprintf(stderr, "L%d: can't print, stack empty\n", ln);
 		exit(EXIT_FAILURE);
 	}
-	if (*stack)
-		printf("%d\n", (*stack)->n);
+	printf("%d\n", (*stack)->n);
 }
