eea50c10c5e53a2666e49ddd0af9678535a2f2eb
hippocampus3282 <3282@holbertonschool.com>
2021-09-22 11:04:09 -0700

    Project 0x19 C - Stack, Queues. LIFO & FIFO

--- pint.c
+++ pint.c
@@ -4,5 +4,6 @@ void pint(stack_t **stack, unsigned int ln __attribute__((unused)))
 {
 	if (!stack)
 		fprintf(stderr, "Stack empty\n");
-	printf("%d\n", (*stack)->n);
+	if (*stack)
+		printf("%d\n", (*stack)->n);
 }
