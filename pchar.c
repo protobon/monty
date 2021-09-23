3c28a37b61f33748bd29933a1e38481f61593e08
hippocampus3282 <3282@holbertonschool.com>
2021-09-23 15:52:19 -0700

    fix error messages

--- pchar.c
+++ pchar.c
@@ -1,20 +1,17 @@
 #include "monty.h"
+#include <ctype.h>
 
 void pchar(stack_t **stack, unsigned int ln)
 {
-	int a;
-
 	if (!stack || !(*stack))
 	{
-		fprintf(stderr, "L%d: can't pchar, value out of range\n", ln);
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", ln);
 		exit (EXIT_FAILURE);
 	}
-	a = (*stack)->n;
-	if (a >= 32 && a <= 127)
-		printf("%c\n", a);
-	else
+	if (isascii((*stack)->n) == 0)
 	{
-		fprintf(stderr, "L%d: can't pchar, stack empty\n", ln);
+		fprintf(stderr, "L%d: can't pchar, value out of range\n", ln);
 		exit(EXIT_FAILURE);
 	}
+	printf("%c\n", (*stack)->n);
 }
