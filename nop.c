b0eded7b9552e463b351f3672a4d998e2139c918
hippocampus3282 <3282@holbertonschool.com>
2021-09-23 06:05:32 -0700

    add nop opcode

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+  * nop - does nothing
+  * @stack: head of stack
+  * @ln: line count
+  */
+void nop(stack_t **stack, unsigned int ln)
+{
+	stack = stack;
+	ln = ln;
+}
