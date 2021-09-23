e0b424e59bdd63773d85b7230a7beeddca68bc35
hippocampus3282 <3282@holbertonschool.com>
2021-09-23 16:39:18 -0700

    fix corner case, add comments

--- rotl.c
+++ rotl.c
@@ -1,11 +1,22 @@
 #include "monty.h"
 
+/**
+  * rotl - rotates stack to the top
+  * @stack: head of stack
+  * @ln: line number, unused
+  * Description: top element becomes last one,
+  * second element becomes first, rotl never fails
+  */
 void rotl(stack_t **stack, unsigned int ln __attribute__((unused)))
 {
 	stack_t *tmp = *stack, *last = *stack;
 
+	if (!stack || !(*stack) || !(*stack)->next)
+		return;
+
 	while (last->next)
 		last = last->next;
+
 	last->next = tmp;
 	*stack = (*stack)->next;
 	tmp->prev = last;
